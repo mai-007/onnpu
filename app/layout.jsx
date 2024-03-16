77bb22fe593fa2602804786a3f3650ab1ebc1293
m.k <m.kotou0715@gmail.com>
2024-03-17 08:49:07 +0900

    favicon/meta修正/ogp追加

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,11 +4,34 @@ import Analytics from '../utils/Analytics';
 import "@/styles/globals.css";
 
 
+const siteName= 'ONNPU|ト音記号とへ音記号マスターへの近道！ピアノ初心者のためのブラウザゲーム';
+const description = '音符を読むのが苦手？ ト音記号とへ音記号を楽しく覚えられるブラウザゲームで、楽譜をスラスラ読めるようになりましょう!';
+const url = 'https://onnpu.com';
+
 
 export const metadata = {
-  title: "ONNPU|ト音記号とへ音記号マスターへの近道！ピアノ初心者必見のブラウザゲーム",
-  description: "音符を読むのが苦手？ ト音記号とへ音記号を楽しく覚えられるブラウザゲームで、楽譜をスラスラ読めるようになりましょう!"
-};
+  title: {
+    default: siteName,
+    /** `next-seo`の`titleTemplate`に相当する機能 */
+    template: `%s - ${siteName}`,
+  },
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description,
+    // site: '@サイト用アカウントのTwitterID',
+    // creator: '@作者のTwitterID',
+  },
+}
 
 export default function RootLayout({ children }) {
   return (
